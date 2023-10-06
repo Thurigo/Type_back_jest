b69cb3fa2a9aee9992aeb2f67ec35d11108c62f9
Thurigo <arthur.spier@gmail.com>
2023-10-06 12:58:57 -0300

    Criado teste para batalha tenatr com a sorte

--- test/forca.spec.ts
+++ test/forca.spec.ts
@@ -1,7 +1,7 @@
 import { JogadorClasse, Guerreiro, Bardo, Mago, Ladrao, } from     '../../type_back/src/Playerclass';
 
 import { Inimigo,criarListaDeInimigo } from     '../../type_back/src/inimigo';
-import { Atacar, Fugir, Papear } from '../../type_back/src/Batalha';
+import { Atacar, Fugir, Papear, TentarSorte } from '../../type_back/src/Batalha';
 import { Randon_numero } from '../../type_back/src/randon_number';
 
 describe('Teste batalha ataque', () => {
@@ -22,7 +22,7 @@ describe('Teste batalha ataque', () => {
     })
 
 
-    test('Teste de batalha de agilidade' ,() => {
+    test('Teste de batalha de carisma' ,() => {
         const Player = new JogadorClasse('Thurigo', 3,3 ,new Guerreiro)
         const lista = criarListaDeInimigo(8)
         const dado = Randon_numero(1,3);
@@ -30,5 +30,13 @@ describe('Teste batalha ataque', () => {
         expect(test_Player.classe.defesa).toBe(0);
     })
 
-})
+    test('Teste de batalha de sorte' ,() => {
+        const Player = new JogadorClasse('Thurigo', 3,3 ,new Guerreiro)
+        const lista = criarListaDeInimigo(8)
+        const dado = Randon_numero(1,3);
+        const test_Player = TentarSorte(Player,lista[dado]);
+        expect(test_Player.classe.sorte).toBe(1);
+        expect(test_Player.classe.sorte).toBe(2);
+});
 
+})
