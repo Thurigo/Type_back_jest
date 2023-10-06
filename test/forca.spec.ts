f8f88b700da395bc860c177558fa145e5c947643
Thurigo <arthur.spier@gmail.com>
2023-10-06 02:06:50 -0300

    Criado teste para batalha Fugir, Já testado com jest

--- test/forca.spec.ts
+++ test/forca.spec.ts
@@ -1,11 +1,17 @@
 import { JogadorClasse, Guerreiro, Bardo, Mago, Ladrao, } from     '../../type_back/src/Playerclass';
 
-import { Atacar } from '../../type_back/src/Batalha';
+import { Atacar, Fugir } from '../../type_back/src/Batalha';
 
 describe('Teste batalha ataque', () => {
-    test('Teste de batalha de desefa' ,() => {
+    test('Teste de batalha de defesa' ,() => {
         const Player = new JogadorClasse('Thurigo', 3,3 ,new Guerreiro)
         const test_Player = Atacar(Player);
-        expect(test_Player.classe.defesa).toBe(0);
+        expect(test_Player.classe.defesa).toBe(5);
+    })
+
+    test('Teste de batalha de agilidade' ,() => {
+        const Player = new JogadorClasse('Thurigo', 3,3 ,new Guerreiro)
+        const test_Player = Fugir(Player);
+        expect(test_Player.classe.defesa).toBe(5);
     })
 })
