c08f79c4875b494f3475dcd1ed08d8d8897dcb84
Thurigo <arthur.spier@gmail.com>
2023-10-06 16:06:02 -0300

    Criado teste para Drop de gold por Andar

--- test/Batalha.spec.ts
+++ test/Batalha.spec.ts
@@ -1,44 +1,50 @@
-import { JogadorClasse, Guerreiro, Bardo, Mago, Ladrao, } from     '../../type_back/src/Playerclass';
+import { JogadorClasse, Guerreiro, Bardo, Mago, Ladrao, } from '../../type_back/src/Playerclass';
 
-import { Inimigo,criarListaDeInimigo } from     '../../type_back/src/inimigo';
-import { Atacar, Fugir, Papear, TentarSorte } from '../../type_back/src/Batalha';
+import { Inimigo, criarListaDeInimigo } from '../../type_back/src/inimigo';
+import { Atacar, Fugir, GoldAndar, Papear, TentarSorte } from '../../type_back/src/Batalha';
 import { Randon_numero } from '../../type_back/src/randon_number';
 
 describe('Teste batalha ataque', () => {
-   
 
-    test('Teste de batalha de defesa' ,() => {
-        const Player = new JogadorClasse('Thurigo', 3,3 ,new Guerreiro)
+    test('teste gold andar', () => {
+        const Player = new JogadorClasse('Thurigo', 3, 3, new Guerreiro)
+        const Andartest = GoldAndar(Player);
+
+        expect(Andartest.gold).toBe(8);
+    })
+
+    test('Teste de batalha de defesa', () => {
+        const Player = new JogadorClasse('Thurigo', 3, 3, new Guerreiro)
         const lista = criarListaDeInimigo(8);
-        const dado = Randon_numero(1,3);
-        const test_Player = Atacar(Player,lista[dado]);
+        const dado = Randon_numero(1, 3);
+        const test_Player = Atacar(Player, lista[dado]);
         expect(test_Player.classe.defesa).toBe(0);
     })
 
-    test('Teste de batalha de agilidade' ,() => {
-        const Player = new JogadorClasse('Thurigo', 3,3 ,new Guerreiro)
+    test('Teste de batalha de agilidade', () => {
+        const Player = new JogadorClasse('Thurigo', 3, 3, new Guerreiro)
         const lista = criarListaDeInimigo(8);
-        const dado = Randon_numero(1,3);
-        const test_Player = Fugir(Player,lista[dado]);
+        const dado = Randon_numero(1, 3);
+        const test_Player = Fugir(Player, lista[dado]);
         expect(test_Player.classe.defesa).toBe(0);
     })
 
 
-    test('Teste de batalha de carisma' ,() => {
-        const Player = new JogadorClasse('Thurigo', 3,3 ,new Guerreiro)
+    test('Teste de batalha de carisma', () => {
+        const Player = new JogadorClasse('Thurigo', 3, 3, new Guerreiro)
         const lista = criarListaDeInimigo(8);
-        const dado = Randon_numero(1,3);
-        const test_Player = Papear(Player,lista[dado]);
+        const dado = Randon_numero(1, 3);
+        const test_Player = Papear(Player, lista[dado]);
         expect(test_Player.classe.defesa).toBe(0);
     })
 
-    test('Teste de batalha de sorte' ,() => {
-        const Player = new JogadorClasse('Thurigo', 3,3 ,new Guerreiro)
+    test('Teste de batalha de sorte', () => {
+        const Player = new JogadorClasse('Thurigo', 3, 3, new Guerreiro)
         const lista = criarListaDeInimigo(8);
-        const dado = Randon_numero(1,3);
-        const test_Player = TentarSorte(Player,lista[dado]);
+        const dado = Randon_numero(1, 3);
+        const test_Player = TentarSorte(Player, lista[dado]);
         expect(test_Player.classe.sorte).toBe(1);
         expect(test_Player.classe.sorte).toBe(2);
-});
+    });
 
 })
